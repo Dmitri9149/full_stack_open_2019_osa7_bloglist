55fb01e1674cc4c11d519bc08dc627e80ac25125
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-06-30 19:42:19 +0300

    changes to handle the store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import notificationReducer from './reducers/notificationReducer'
+import { createMessage } from './reducers/notificationReducer'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(notificationReducer)
+
+const renderApp = () => {
+  ReactDOM.render(
+    <App store={store}/>,
+    document.getElementById('root')
+  )
+}
+
+renderApp()
+store.subscribe(renderApp)
